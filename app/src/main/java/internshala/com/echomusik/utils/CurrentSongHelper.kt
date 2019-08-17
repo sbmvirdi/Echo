534670adbc6efef5b9445334fcf9aefab248c78f
shubam virdi <shubhamvirdic3@gmail.com>
2019-08-17 21:45:13 +0530

    Initial commit

---
+++ app/src/main/java/internshala/com/echomusik/utils/CurrentSongHelper.kt
@@ -0,0 +1,26 @@
+package internshala.com.echomusik.utils
+
+
+
+class CurrentSongHelper {
+
+
+    var songArtist: String? = null
+
+    var songTitle: String? = null
+
+    var songPath: String? = null
+
+    var currentPosition: Int = 0
+
+    var songId: Long = 0
+
+    var isPlaying: Boolean? = false
+
+    var isLoop: Boolean = false
+
+    var isShuffle: Boolean = false
+
+    var TrackPosition: Int = 0
+
+}
